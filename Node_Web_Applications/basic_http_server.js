b08e4cd81b06c204cedb7d459b32e80a950b360e
zbw322092 <bz4e14@soton.ac.uk>
2016-11-06 12:17:23 +0800

    REST service POST

--- Node_Web_Applications/basic_http_server.js
+++ Node_Web_Applications/basic_http_server.js
@@ -1,8 +1,20 @@
 var http = require('http');
+// var server = http.createServer(function(req, res) {
+// 	var body = "Hello World";
+// 	res.setHeader('Content-Length', body.length);
+// 	res.setHeader('Content-type', 'text/plain');
+// 	res.write(body);
+// 	res.end();
+// });
+
 var server = http.createServer(function(req, res) {
-	var body = "Hello World";
+	var url = "http://cn.bing.com/"
+	var body = '<p>Redirecting to <a href="' + url + '">'
+         + url + '</a></p>';
+  res.setHeader('Location', url);
 	res.setHeader('Content-Length', body.length);
-	res.setHeader('Content-type', 'text/plain');
+	res.setHeader('Content-type', 'text/html');
+	res.statusCode = 302;
 	res.write(body);
 	res.end();
 });
