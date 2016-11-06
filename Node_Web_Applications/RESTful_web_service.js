2fcb2dfde056e46ac8a56ad1dd605aee88c3aa3b
zbw322092 <bz4e14@soton.ac.uk>
2016-11-06 15:14:58 +0800

    REST service DELETE

--- Node_Web_Applications/RESTful_web_service.js
+++ Node_Web_Applications/RESTful_web_service.js
@@ -17,6 +17,40 @@ var server = http.createServer(function(req, res) {
 				res.end('OK\n');
 			});
 			break;
+
+		case 'GET':
+			// 
+			var body = items.map(function(item, i) {
+				return i + ') ' + item;
+			}).join('\n');
+
+			res.setHeader('Content-Length', Buffer.byteLength(body));
+			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+			res.end(body);
+
+			// items.forEach(function(item, i) {
+			// 	res.write(i + ')' + item + '\n');
+			// });
+			// res.end();
+			break;
+
+		case 'DELETE':
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10);
+
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id');				
+			} else if (!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found');
+			} else {
+				items.splice(i, 1);
+				res.end('OK\n');
+			}
+			break;
+
 	}
 });
 
+server.listen(8000);
