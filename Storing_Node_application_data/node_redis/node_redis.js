c626434c66025ad7bd617ccc69910c5eaf404d87
Bowen Zhong <bz4e14@soton.ac.uk>
2016-11-09 11:02:04 +0800

    redis channel

--- Storing_Node_application_data/node_redis/node_redis.js
+++ Storing_Node_application_data/node_redis/node_redis.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var redis = require('redis');
 var client = redis.createClient(6379, '127.0.0.1');
 
@@ -39,8 +40,41 @@ client.lrange('task', 0, -1, function(err, items) {
 });
 
 
+client.sadd('ip_addresses', '204.10.37.96', redis.print); 
+client.sadd('ip_addresses', '204.10.37.96', redis.print); 
+client.sadd('ip_addresses', '72.32.231.8', redis.print);
+client.smembers('ip_addresses', function(err, members) {
+	if (err) throw err;
+	console.log(members);
+});
 
+var server = net.createServer(function(socket) {
+	var subscriber;
+	var publisher;
 
+	socket.on('connect', function() {
+		subscriber = redis.createClient();
+		subscriber.subscribe('main_chat_room');
+
+		subscriber.on('message', function(channel, message) {
+			socket.write('Channel ' + channel + ': ' + message);
+		});
+
+		publisher = redis.createClient();
+	});
+
+	socket.on('data', function() {
+		publisher.publish('main_chat_room', data);
+	});
+
+	socket.on('end', function() {
+		subscriber.unsubscribe('main_chat_room');
+		subscriber.end();
+		publisher.end();
+	});
+});
+
+server.listen(3000);
 
 
 
