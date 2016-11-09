d30fd7dd3f0c01273e922978ee20c0a598a34f85
zbw322092 <bz4e14@soton.ac.uk>
2016-11-09 21:51:56 +0800

    routeing middleware

--- connect/routing_middleware_component.js
+++ connect/routing_middleware_component.js
@@ -3,13 +3,23 @@ var router = require('./middleware/router');
 
 var routes = {
 	GET: {
-		'/users': function(req, res) {
+		'/users': function (req, res) {
 			res.end('tobi, loki, ferret');
 		},
-		{
-			'/users/:id': function(req, res, id) {
-				res.end('user ' + id);
-			}
+		'/users/:id': function(req, res, id) {
+			res.end('user ' + id);
+		}
+	},
+
+	DELETE: {
+		'/user/:id' : function(req, res, id) {
+			res.end('delete user ' + id);
 		}
 	}
-}
+};
+
+connect()
+	.use(router(routes));
+	.listen(3000);
+
+	
