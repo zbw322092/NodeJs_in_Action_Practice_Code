3ae3c54ba0fea3fe40c0314a81675156618bae4a
Bowen Zhong <bz4e14@soton.ac.uk>
2016-11-09 16:16:35 +0800

    middleware

---
+++ connect/connect_application.js
@@ -0,0 +1,17 @@
+var connect = require('connect');
+var app = connect();
+
+function logger(req, res, next) {
+	console.log('%s %s', req.method, req.url);
+	next();
+}
+
+function hello(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+}
+
+connect()
+	.use(logger)
+	.use(hello)
+	.listen(3000);
